e4d567c1f6fb5cd2a500506bca36aeb47179fbbe
Parth Tiwari <parthtiwari98@gmail.com>
2025-10-01 01:17:58 +0530

    LInkedList Constructor added

--- Core Skills/LinkedListImplementation.js
+++ Core Skills/LinkedListImplementation.js
@@ -1,12 +1,26 @@
 class Node{
-    constructor(){
-        
+    constructor(value){
+        this.value = value;
+        this.next = null;
     }
 }
 
-
+/*
+* constructor, size, head, tail, isEmpty, addLast, addFirst, removeLast, 
+* removeFirst, remove Element, indexOf, elementAt, addAt, removeAt, findMiddle, clean, get, 
+* rotateListRight, reverse
+ */
 class Linkedlist{
-    constructor(value){
 
+   constructor(listOfValues){
+    this.head = null
+    this.tail = null;
+    this.length = 0;
+
+    if( listOfValues instanceof Array){
+        for(const value of listOfValues){
+            this.addLast(value);
+        }
     }
+   }
 }
