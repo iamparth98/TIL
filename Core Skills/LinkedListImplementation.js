a3bda174d597875902d32e4b31c23f3ce7e05ae8
Parth Tiwari <parthtiwari98@gmail.com>
2025-10-13 14:00:36 +0530

    added easy features

--- Core Skills/LinkedListImplementation.js
+++ Core Skills/LinkedListImplementation.js
@@ -1,26 +1,38 @@
-class Node{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 /*
-* constructor, size, head, tail, isEmpty, addLast, addFirst, removeLast, 
-* removeFirst, remove Element, indexOf, elementAt, addAt, removeAt, findMiddle, clean, get, 
-* rotateListRight, reverse
+ * constructor, size, head, tail, isEmpty, addLast, addFirst, removeLast,
+ * removeFirst, remove Element, indexOf, elementAt, addAt, removeAt, findMiddle, clean, get,
+ * rotateListRight, reverse
  */
-class Linkedlist{
-
-   constructor(listOfValues){
-    this.head = null
+class Linkedlist {
+  constructor(listOfValues) {
+    this.head = null;
     this.tail = null;
     this.length = 0;
 
-    if( listOfValues instanceof Array){
-        for(const value of listOfValues){
-            this.addLast(value);
-        }
+    if (listOfValues instanceof Array) {
+      for (const value of listOfValues) {
+        this.addLast(value);
+      }
     }
-   }
+  }
+
+  size() {
+    return this.length;
+  }
+  headNode() {
+    return this.headNode?.value ?? null;
+  }
+  tailNode() {
+    return this.tailNode?.value ?? null;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
 }
